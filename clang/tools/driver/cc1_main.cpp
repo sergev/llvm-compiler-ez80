pawosm-arm/llvm-project

[cc1] Initialize all of the passes when -mllvm is passed so that -print-before/after can be parsed correctly.

clang/tools/driver/cc1_main.cpp
@@ -27,10 +27,12 @@
 #include "clang/FrontendTool/Utils.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Config/llvm-config.h"
+#include "llvm/InitializePasses.h"
 #include "llvm/LinkAllPasses.h"
 #include "llvm/Option/Arg.h"
 #include "llvm/Option/ArgList.h"
 #include "llvm/Option/OptTable.h"
+#include "llvm/Pass.h"
 #include "llvm/Support/BuryPointer.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/ErrorHandling.h"
@@ -233,6 +235,26 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
   DiagsBuffer->FlushDiagnostics(Clang->getDiagnostics());
   if (!Success)
     return 1;
+
+  if (!Clang->getFrontendOpts().LLVMArgs.empty()) {
+    // Initialize codegen and IR passes used by llc so that the -print-after,
+    // -print-before, and -stop-after options work.
+    llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
+    llvm::initializeCore(Registry);
+    llvm::initializeCodeGen(Registry);
+    llvm::initializeLoopStrengthReducePass(Registry);
+    llvm::initializeLowerIntrinsicsPass(Registry);
+    llvm::initializeEntryExitInstrumenterPass(Registry);
+    llvm::initializePostInlineEntryExitInstrumenterPass(Registry);
+    llvm::initializeUnreachableBlockElimLegacyPassPass(Registry);
+    llvm::initializeConstantHoistingLegacyPassPass(Registry);
+    llvm::initializeScalarOpts(Registry);
+    llvm::initializeVectorization(Registry);
+    llvm::initializeScalarizeMaskedMemIntrinPass(Registry);
+    llvm::initializeExpandReductionsPass(Registry);
+    llvm::initializeHardwareLoopsPass(Registry);
+    llvm::initializeTransformUtils(Registry);
+  }
 
   // Execute the frontend actions.
   {

sergev/llvm-compiler-ez80

clang/tools/driver/cc1_main.cpp
@@ -27,11 +27,13 @@
 #include "clang/FrontendTool/Utils.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Config/llvm-config.h"
+#include "llvm/InitializePasses.h"
 #include "llvm/LinkAllPasses.h"
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/Option/Arg.h"
 #include "llvm/Option/ArgList.h"
 #include "llvm/Option/OptTable.h"
+#include "llvm/Pass.h"
 #include "llvm/Support/BuryPointer.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/ErrorHandling.h"
@@ -240,6 +242,26 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
   if (!Success) {
     Clang->getDiagnosticClient().finish();
     return 1;
+  }
+
+  if (!Clang->getFrontendOpts().LLVMArgs.empty()) {
+    // Initialize codegen and IR passes used by llc so that the -print-after,
+    // -print-before, and -stop-after options work.
+    llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
+    llvm::initializeCore(Registry);
+    llvm::initializeCodeGen(Registry);
+    llvm::initializeLoopStrengthReducePass(Registry);
+    llvm::initializeLowerIntrinsicsPass(Registry);
+    llvm::initializeEntryExitInstrumenterPass(Registry);
+    llvm::initializePostInlineEntryExitInstrumenterPass(Registry);
+    llvm::initializeUnreachableBlockElimLegacyPassPass(Registry);
+    llvm::initializeConstantHoistingLegacyPassPass(Registry);
+    llvm::initializeScalarOpts(Registry);
+    llvm::initializeVectorization(Registry);
+    llvm::initializeScalarizeMaskedMemIntrinLegacyPassPass(Registry);
+    llvm::initializeExpandReductionsPass(Registry);
+    llvm::initializeHardwareLoopsPass(Registry);
+    llvm::initializeTransformUtils(Registry);
   }
 
   // Execute the frontend actions.